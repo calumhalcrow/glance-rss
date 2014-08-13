6db4b7312ed28fb471a09e1b176bbc4f26bdba11
Calum Halcrow <calumhalcrow@gmail.com>
2014-08-13 20:38:58 +0800

    PureCSS.

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/pure/pure-min.css');
+app.import('vendor/pure/grids-responsive-min.css');
+
 module.exports = app.toTree();
